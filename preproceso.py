30f4891f97f51f8a2b73ccfa77d3ce0919a1c27d
MCenriquezR <38733127+MCenriquezR@users.noreply.github.com>
2019-10-18 07:33:41 -0700

    Update preproceso.py

--- preproceso.py
+++ preproceso.py
@@ -1,10 +1,10 @@
 import re
 from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk import download, Text
-from nltk.util import ngrams
+from nltk import download
 from random import random
-from collections import Counter
 from nltk.lm.preprocessing import padded_everygram_pipeline
+from nltk.lm.api import LanguageModel
+from nltk import compat
 from nltk.lm import MLE
 download('punkt')
 
@@ -27,57 +27,35 @@ def limpia_donquijote(text):
 
     return text
 
-def ngramaFecuencias(ngramas):
-    """frecuencias = {}
-    for ngrama in ngramas:
-        if ngrama in frecuencias:
-            frecuencias[ngrama] += 1
-        else:
-            frecuencias[ngrama] = 1"""
-    return Counter(ngramas)
 
-def oracionVerosimilitudLambda(oracion, n, lambdaC = 1):
-    def probabilidad():
-        pass
+@compat.python_2_unicode_compatible
+class MLE(LanguageModel):
+    """Class for providing MLE ngram model scores.
 
-    pass
+    Inherits initialization from BaseNgramModel.
+    """
+
+    def unmasked_score(self, word, context=None):
+        """Add-one smoothing: Laplace.
+
+        """
+        counts = self.context_counts(context)
+        word_count = counts[word]
+        norm_count = counts.N()
+        return (word_count + 1) / (norm_count + len(self.vocab) + 1)
 
 if __name__ == "__main__":
     text = ""
     with open("datos/DonQuijote.txt", 'rt', \
               encoding='utf8') as file:
         text = file.read()
-
     text = limpia_donquijote(text)
-
     texto_tokenizado = [list(map(str.lower, word_tokenize(oracion)))
                       for oracion in sent_tokenize(text)]
-    for i, key in enumerate(texto_tokenizado):
-        if i == 0:
-            break
-        print(key)
-    #frecuencias = ngramaFecuencias(ngrams(words, 4))
-
-    #for i, (key, value) in enumerate(frecuencias.items()):
-    #    if i == 25:
-    #        break
-    #    print(key, value)
     n = 4
     datos_entrenamiento, sentencias_con_etiquetas = padded_everygram_pipeline(n, texto_tokenizado)
 
     model = MLE(n)  # oracionVerosimilitud
     model.fit(datos_entrenamiento, sentencias_con_etiquetas)
-    texto_generado = ""
-    for elem in model.generate(25, None, random()):
-        if elem != "</s>":
-            texto_generado += " " + elem
+    texto_generado = model.generate(35, None, random())
     print(texto_generado)
-    
-    with open("datos/output.txt", 'w') as file:
-        file.write(text)
-
-
-
-
-    
-    
